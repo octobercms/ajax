2214d4bcf856ac2241cdcd1229f852d784de44db
Sam Georges <realsamgeorges@gmail.com>
2022-06-14 13:25:48 +1000

    init $.oc

--- src/extras/migrate.js
+++ src/extras/migrate.js
@@ -2,6 +2,10 @@
 export class Migrate
 {
     bind() {
+        if ($.oc === undefined) {
+            $.oc = {};
+        }
+
         $.oc.flashMsg = window.oc.flashMsg;
         $.oc.stripeLoadIndicator = window.oc.progressBar;
     }
