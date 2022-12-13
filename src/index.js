ded8bf32c111bb87d985a8da1ed9484d46ea7a99
October CMS <hello@octobercms.com>
2022-12-13 13:20:14 +1100

    Adds default exports

--- src/index.js
+++ src/index.js
@@ -24,11 +24,29 @@ export {
 }
 
 export const
-    ajax = () => AjaxRequest.send(...arguments),
-    request = () => AjaxFramework.requestElement(...arguments),
-    parseJSON = () => AjaxFramework.parseJSON(...arguments),
-    flashMsg = () => AjaxExtras.flashMsg(...arguments),
-    progressBar = () => AjaxExtras.progressBar(...arguments),
-    attachLoader = () => AjaxExtras.attachLoader(...arguments),
-    useTurbo = () => AjaxTurbo.isEnabled(...arguments),
-    visit = () => AjaxTurbo.visit(...arguments);
+    ajax = (handler, options) => AjaxRequest.send(handler, options),
+    request = (element, handler, options) => AjaxFramework.requestElement(element, handler, options),
+    parseJSON = (json) => AjaxFramework.parseJSON(json),
+    flashMsg = (options) => AjaxExtras.flashMsg(options),
+    progressBar = () => AjaxExtras.progressBar(),
+    attachLoader = () => AjaxExtras.attachLoader(),
+    useTurbo = () => AjaxTurbo.isEnabled(),
+    visit = (location, options) => AjaxTurbo.visit(location, options)
+;
+
+export default {
+    AjaxFramework,
+    AjaxRequest,
+    AjaxExtras,
+    AjaxTurbo,
+    AssetManager,
+    Events,
+    ajax,
+    request,
+    parseJSON,
+    flashMsg,
+    progressBar,
+    attachLoader,
+    useTurbo,
+    visit
+};
